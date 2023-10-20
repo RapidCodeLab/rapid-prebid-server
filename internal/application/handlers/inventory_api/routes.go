cb23df81c7cc1c285d2842377c72504e6f8ae35b
RapidCodeLab <rapidcodelab.mx@gmail.com>
2023-10-20 14:32:18 +0300

    route path

--- internal/application/handlers/inventory_api/routes.go
+++ internal/application/handlers/inventory_api/routes.go
@@ -3,15 +3,15 @@ package inventoryapi_handler
 import "github.com/buaazp/fasthttprouter"
 
 func (h *Handler) LoadRoutes(r *fasthttprouter.Router) {
-	r.GET("", h.HandleReadAllInventories)
-	r.POST("", h.HandleCreateInventory)
-	r.GET("", h.HandleReadInventory)
-	r.POST("", h.HandleUpdateInventory)
-	r.GET("", h.HandleDeleteInventory)
+	r.GET("/inventory-api/inventories", h.HandleReadAllInventories)
+	r.POST("/inventory-api/inventory/create", h.HandleCreateInventory)
+	r.GET("/inventory-api/inventory/read/:id", h.HandleReadInventory)
+	r.POST("/inventory-api/inventory/update", h.HandleUpdateInventory)
+	r.GET("/inventory-api/inventory/delete/:id", h.HandleDeleteInventory)
 	//
-	r.GET("", h.HandleReadAllEntities)
-	r.POST("", h.HandleCreateEntity)
-	r.GET("", h.HandleReadEntity)
-	r.POST("", h.HandleUpdateEntity)
-	r.GET("", h.HandleDeleteEntity)
+	r.GET("/inventory-api/entities", h.HandleReadAllEntities)
+	r.POST("/inventory-api/entity/create", h.HandleCreateEntity)
+	r.GET("/inventory-api/entity/read/:id", h.HandleReadEntity)
+	r.POST("/inventory-api/entity/update", h.HandleUpdateEntity)
+	r.GET("/inventory-api/entity/delete/:id", h.HandleDeleteEntity)
 }
